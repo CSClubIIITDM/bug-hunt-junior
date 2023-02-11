03417dfcc8d93633559e576168b0e34ccfe4cff8
Dharun Thota <dharunth@gmail.com>
2023-02-11 16:26:45 +0530

    more bugs

--- Python/s4.py
+++ Python/s4.py
@@ -1,14 +1,18 @@
 # Program: Check if an integer is a power of 2
 
 def is_power_of_two(n):
-    if n <= 0:
+    if n < 0 or n % 2 != 0:
         return False
     else:
-        i = 1
-        while i % 2 != 0:
-            if 2 ** i == n:
-                return n
-        return (n&(n-1)) == 1
+        n = n / 2
+        if n < 1:
+            return False
+        elif n == 1:
+            return True
+        else:
+            is_power_of_two(n)
+    #return (n & (n-1)) == 1
+
 
 num = 64
 if is_power_of_two(num):
