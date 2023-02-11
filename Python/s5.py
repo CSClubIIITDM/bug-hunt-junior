2dad967c6747681fad723b405088856b9c3ad48d
pratyek <125120600+pratyek@users.noreply.github.com>
2023-02-11 16:26:22 +0530

    Update s5.py

--- Python/s5.py
+++ Python/s5.py
@@ -1,9 +1,9 @@
 # Program: How are you doing?
 
 answer1 = input("Are you enjoying Vashisht? ([Y]es/[N]o) ")
-if answer1.lower() == "y" or "yes":
+if answer1.lower() in ["y", "yes"]:
     print("That's great! We hope you're having fun hunting for bugs.")
-elif answer1.lower() == "n" or "no":
+elif answer1.lower() in ["n", "no"]:
     print(":( We're sorry to hear that. We hope you'll have fun soon.")
 else:
     print("404 Not Found!")
@@ -11,9 +11,9 @@ else:
 print()
 
 answer2 = input("Do you want me to print bugs for all Python files? ;) ([Y]es/[N]o) ")
-if answer2.lower() == "y" or "yes":
+if answer2.lower() in ["y", "yes"]:
     print("Nice try :P")
-elif answer2.lower() == "n" or "no":
+elif answer2.lower() in ["n", "no"]:
     print("That's the spirit!")
 else:
     print("404 Not Found!")
