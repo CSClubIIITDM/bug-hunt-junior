a89b5eabc94d02f9b36334df9534bfaf9f6edb3f
Nivedh-Biju <125106178+Nivedh-Biju@users.noreply.github.com>
2023-02-11 15:28:33 +0530

    Update s3.py

--- Python/s3.py
+++ Python/s3.py
@@ -4,7 +4,7 @@ def sort_numbers(numbers):
     for i in range(len(numbers)):
         for j in range(i + 1, len(numbers)):
             if numbers[i] > numbers[j]:
-                temp = numbers[i]
+                temp = numbers[j]
                 numbers[j] = numbers[i]
                 numbers[i] = temp
     return numbers
